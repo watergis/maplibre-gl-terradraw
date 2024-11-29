0b79a4938f9fed0c960e8868784e30c3bfd8181d
Jin Igarashi <igarashi.jin@water-gis.com>
2024-11-29 22:09:32 +0900

    doc: renamed customise to examples, and improved UI. add default data for main demo page

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -32,17 +32,20 @@ export const load: LayoutServerLoad = async () => {
 			{
 				href: '/customise/drawing-option',
 				title: 'Customise drawing options',
+				image: '/assets/images/customise-draw-option.webp',
 				description:
 					'Change default drawing options to disable some editing functionalities of polygon mode.'
 			},
 			{
 				href: '/customise/select-event',
 				title: 'Subscribe select event of TerraDraw',
+				image: '/assets/images/subscribe-selectevent.webp',
 				description: 'Use TerraDraw API to subscribe an event.'
 			},
 			{
 				href: '/customise/add-geojson',
 				title: 'Adding default GeoJSON feature to TerraDraw',
+				image: '/assets/images/add-geojson.webp',
 				description: 'Add GeoJSON features to TerraDraw as default by using addFeatures function.'
 			}
 		],

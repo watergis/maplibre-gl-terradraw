dde5c4d8e122847926731377dfff810c396a25c0
Jin Igarashi <igarashi.jin@water-gis.com>
2024-12-08 11:01:59 +0900

    fix: commit

--- src/lib/interfaces/index.ts
+++ src/lib/interfaces/index.ts
@@ -1,5 +1,6 @@
 export * from './TerradrawControlOptions.js';
 export * from './EventTypes.js';
+export * from './MeasureControlOptions.js';
 export * from './ModeOptions.js';
 export * from './TerradrawMode.js';
 export * from './TerradrawModeClass.js';

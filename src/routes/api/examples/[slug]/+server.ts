b1988276caf6021719791b1681a01a3aa6ecb6bd
Jin Igarashi <igarashi.jin@water-gis.com>
2024-12-27 13:19:53 +0900

    fix: changed html to txt

--- src/routes/api/examples/[slug]/+server.ts
+++ src/routes/api/examples/[slug]/+server.ts
@@ -4,7 +4,7 @@ import type { RequestHandler } from './$types.js';
 export const GET: RequestHandler = async ({ params, fetch }) => {
 	const slug = params.slug;
 
-	const filePath = `/assets/examples/${slug}.html`;
+	const filePath = `/assets/examples/${slug}.txt`;
 
 	const res = await fetch(filePath);
 	if (!res.ok) {

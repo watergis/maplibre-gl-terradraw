ff454f869b3bdffc33b5a6fd228f436083344f62
Jin Igarashi <igarashi.jin@water-gis.com>
2025-01-17 22:05:48 +0900

    add swagger-ui-dist to ssr no external

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	ssr: {
-		noExternal: 'maplibre-gl'
+		noExternal: ['maplibre-gl', 'swagger-ui-dist']
 	},
 	server: {
 		fs: {

7608b8b7e07b602f9fbca168bdf475bef5ea9959
Jin Igarashi <igarashi.jin@water-gis.com>
2025-01-17 23:20:51 +0900

    vervel ci

--- eslint.config.js
+++ eslint.config.js
@@ -28,6 +28,6 @@ export default [
 		}
 	},
 	{
-		ignores: ['build/', '.svelte-kit/', 'dist/', 'package/', 'docs/']
+		ignores: ['build/', '.svelte-kit/', 'dist/', 'package/', 'docs/', '.vercel/']
 	}
 ];

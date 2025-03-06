5b1d8c38f5dcd09160c5d7571d196350febcfabf
Jin Igarashi <igarashi.jin@water-gis.com>
2025-03-06 02:44:31 +0000

    fix: improved devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,6 +4,8 @@ set -eo pipefail
 
 # when in a VS Code or GitHub Codespaces devcontainer
 if [ -n "${REMOTE_CONTAINERS}" ] || [ -n "${CODESPACES}" ]; then
+    npm install -g npm-check-updates
+    npm install -g pnpm
     # install dependencies
     pnpm install --frozen-lockfile
 fi

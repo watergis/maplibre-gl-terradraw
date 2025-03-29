34b499ca6ec60324bc516b4d51a74eb73f4498e4
Jin Igarashi <igarashi.jin@water-gis.com>
2025-03-29 11:17:28 +0900

    fix shiki

--- src/routes/CodeBlock.svelte
+++ src/routes/CodeBlock.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { createHighlighterCoreSync } from 'shiki/core';
-	import { createJavaScriptRegexEngine } from 'shiki/engine/javascript';
+	import { createOnigurumaEngine } from 'shiki/engine/oniguruma';
 	// Themes
 	// https://shiki.style/themes
 	import themeDarkPlus from 'shiki/themes/dark-plus.mjs';
@@ -10,15 +10,7 @@
 	import css from 'shiki/langs/css.mjs';
 	import html from 'shiki/langs/html.mjs';
 	import js from 'shiki/langs/javascript.mjs';
-
-	// https://shiki.style/guide/sync-usage
-	const shiki = createHighlighterCoreSync({
-		engine: createJavaScriptRegexEngine(),
-		// Implement your import theme.
-		themes: [themeDarkPlus],
-		// Implement your imported and supported languages.
-		langs: [console, html, css, js]
-	});
+	import { untrack } from 'svelte';
 
 	interface CodeBlockProps {
 		code?: string;
@@ -38,44 +30,57 @@
 	let {
 		code = '',
 		lang = 'console',
-		theme = 'dark-plus'
+		theme = 'dark-plus',
 		// Base Style Props
-		// base = 'overflow-hidden',
-		// rounded = 'rounded-sm',
-		// shadow = '',
-		// classes = '',
-		// // Pre Style Props
-		// preBase = '',
-		// prePadding = '[&>pre]:p-4',
-		// preClasses = ''
+		base = 'overflow-hidden',
+		rounded = 'rounded-sm',
+		shadow = '',
+		classes = '',
+		// Pre Style Props
+		preBase = '',
+		prePadding = '[&>pre]:p-4',
+		preClasses = ''
 	}: CodeBlockProps = $props();
 
 	// Shiki convert to HTML
-	const generatedHtml = shiki.codeToHtml(code, { lang, theme });
+	let generatedHtml = $state('');
 
-	// let copied = $state(false);
-	// let timeout: ReturnType<typeof setTimeout>;
+	let copied = $state(false);
+	let timeout: ReturnType<typeof setTimeout>;
 
-	// const copyToClipboard = () => {
-	// 	navigator.clipboard.writeText(code).then(() => {
-	// 		copied = true;
-	// 		clearTimeout(timeout);
-	// 		timeout = setTimeout(() => (copied = false), 2000);
-	// 	});
-	// };
+	const copyToClipboard = () => {
+		navigator.clipboard.writeText(code).then(() => {
+			copied = true;
+			clearTimeout(timeout);
+			timeout = setTimeout(() => (copied = false), 2000);
+		});
+	};
+
+	$effect(() => {
+		untrack(async () => {
+			const engine = await createOnigurumaEngine(import('shiki/wasm'));
+
+			// https://shiki.style/guide/sync-usage
+			const shiki = createHighlighterCoreSync({
+				engine,
+				// Implement your import theme.
+				themes: [themeDarkPlus],
+				// Implement your imported and supported languages.
+				langs: [console, html, css, js]
+			});
+			generatedHtml = shiki.codeToHtml(code, { lang, theme });
+		});
+	});
 </script>
 
-<!-- <div class="group relative {base} {rounded} {shadow} {classes} {preBase} {prePadding} {preClasses}"> -->
-<!-- <button
+<div class="group relative {base} {rounded} {shadow} {classes} {preBase} {prePadding} {preClasses}">
+	<button
 		class="absolute top-2 right-2 bg-white/10 hover:bg-white/20 text-sm text-white px-2 py-1 rounded transition transition-opacity opacity-0 group-hover:opacity-100"
 		onclick={copyToClipboard}
 	>
 		{copied ? 'Copied' : 'Copy'}
 	</button>
 
-	{@html generatedHtml} -->
-<!-- </div> -->
-
-<!-- Output Shiki's Generated HTML -->
-<!-- eslint-disable svelte/no-at-html-tags -->
-{@html generatedHtml}
+	<!-- eslint-disable svelte/no-at-html-tags -->
+	{@html generatedHtml}
+</div>

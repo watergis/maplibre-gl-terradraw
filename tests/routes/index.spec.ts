2b3698d8be3f97647c538f881252380b6a99f92c
Jin Igarashi <igarashi.jin@water-gis.com>
2025-04-25 11:36:09 +0900

    before each

--- tests/routes/index.spec.ts
+++ tests/routes/index.spec.ts
@@ -1,20 +1,30 @@
 import { AvailableModes } from '../../src/lib/constants/AvailableModes';
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
+import { configureAxe, injectAxe } from 'axe-playwright';
+
+const beforeEach = async (page: Page, url: string) => {
+	await page.goto(url);
+	await injectAxe(page);
+
+	configureAxe(page, {
+		checks: [{ id: 'page-has-heading-one', enabled: false }]
+	});
+};
 
 test.describe('Test without any query params', () => {
 	test('landing page has expected title', async ({ page }) => {
-		await page.goto('/');
+		await beforeEach(page, '/');
 		await expect(page).toHaveTitle(/Maplibre GL Terra Draw/);
 	});
 
 	test('demo page has expected to have map class', async ({ page }) => {
-		await page.goto('/');
+		await beforeEach(page, '/');
 		const mapContainer = page.locator('.map');
 		await expect(mapContainer).toHaveCount(2);
 	});
 
 	test('demo page has expected to have mode buttons', async ({ page }) => {
-		await page.goto('/');
+		await beforeEach(page, '/');
 
 		for (const mode of AvailableModes) {
 			const modeButton = page.getByTestId(mode);
@@ -25,13 +35,13 @@ test.describe('Test without any query params', () => {
 
 test.describe('Test isOpen query param', () => {
 	test('demo page has expected to open tool as default', async ({ page }) => {
-		await page.goto('/?isOpen=open');
+		await beforeEach(page, '/?isOpen=open');
 		const renderButton = page.getByTestId('render');
 		await expect(renderButton).toHaveClass(/enabled/);
 	});
 
 	test('demo page has expected to close tool as default', async ({ page }) => {
-		await page.goto('/?isOpen=close');
+		await beforeEach(page, '/?isOpen=close');
 		const renderButton = page.getByTestId('render');
 		await expect(renderButton).not.toHaveClass(/enabled/);
 	});

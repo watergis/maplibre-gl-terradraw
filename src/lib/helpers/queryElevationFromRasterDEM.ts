9ce6532e782af1fe5f59d396b3511ef804a9fb89
Jin Igarashi <igarashi.jin@water-gis.com>
2025-08-03 20:24:42 +0900

    refactor: resuse terrainrgb instance in queryElevationFromRasterDEM function

--- src/lib/helpers/queryElevationFromRasterDEM.ts
+++ src/lib/helpers/queryElevationFromRasterDEM.ts
@@ -12,22 +12,28 @@ export const queryElevationFromRasterDEM = async (
 	terrainDource?: TerrainSource
 ) => {
 	const promises: Promise<GeoJSONStoreFeatures>[] = [];
+
+	let terrainInstance: TerrainRGB | Terrarium | undefined = undefined;
+	let maxzoom = 15;
+	if (terrainDource) {
+		const url = terrainDource.url;
+		const encoding = terrainDource.encoding ?? 'mapbox';
+		const tileSize = terrainDource.tileSize ?? 512;
+		const minzoom = terrainDource.minzoom ?? 5;
+		maxzoom = terrainDource.maxzoom ?? 15;
+		const tms = terrainDource.tms ?? false;
+		terrainInstance =
+			encoding === 'mapbox'
+				? new TerrainRGB(url, tileSize, minzoom, maxzoom, tms)
+				: new Terrarium(url, tileSize, minzoom, maxzoom, tms);
+	}
+
 	for (const point of points) {
 		promises.push(
 			new Promise((resolve: (feature: GeoJSONStoreFeatures) => void) => {
 				if (point.geometry.type !== 'Point') resolve(point);
-				if (terrainDource) {
-					const url = terrainDource.url;
-					const encoding = terrainDource.encoding ?? 'mapbox';
-					const tileSize = terrainDource.tileSize ?? 512;
-					const minzoom = terrainDource.minzoom ?? 5;
-					const maxzoom = terrainDource.maxzoom ?? 15;
-					const tms = terrainDource.tms ?? false;
-
-					(encoding === 'mapbox'
-						? new TerrainRGB(url, tileSize, minzoom, maxzoom, tms)
-						: new Terrarium(url, tileSize, minzoom, maxzoom, tms)
-					)
+				if (terrainInstance) {
+					terrainInstance
 						.getElevation(point.geometry.coordinates as number[], maxzoom)
 						.then((elevation) => {
 							if (elevation) point.properties.elevation = elevation;

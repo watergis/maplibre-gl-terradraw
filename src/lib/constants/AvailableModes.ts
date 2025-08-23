323f8cb938c77393e393611c2cfb50762c025a72
Jin Igarashi <igarashi.jin@water-gis.com>
2025-08-23 14:51:07 +0900

    docs: add comments

--- src/lib/constants/AvailableModes.ts
+++ src/lib/constants/AvailableModes.ts
@@ -26,6 +26,19 @@ export const AvailableModes = [
 	'download'
 ] as const;
 
+/**
+ * The list of available Terra Draw drawing modes for MaplibreValhallaControl
+ *
+ * - point mode is used to compute isochrone polygons.
+ * - linestring mode is used to calculate routes.
+ *
+ * `render` is a special mode and this is appeared as toggle button to expand/collapse the tool.
+ * If you always want the tool to be visible, `render` can be removed and set `open` property of the constructor as true.
+ *
+ * `delete-selection` button is only appeared, when the select button is active.
+ *
+ * If no drawing feature is in TerraDraw, `delete` and `download` are disabled.
+ */
 export const AvailableValhallaModes = [
 	'render',
 	'linestring',

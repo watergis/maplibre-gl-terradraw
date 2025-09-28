dbcc71d5f60127d98cd3d1149a39dfd82e024a58
Jin Igarashi <igarashi.jin@water-gis.com>
2025-09-28 22:24:22 +0900

    fix: make sure passing measureUnitSymbols to helper functions (#428)

--- src/lib/helpers/calcArea.ts
+++ src/lib/helpers/calcArea.ts
@@ -13,6 +13,7 @@ import { convertAreaUnit } from './convertAreaUnit';
  * @param unitType measure unit type either metric or imperial
  * @param areaPrecision Precision of area value
  * @param forceUnit Default is `auto`. If `auto` is set, unit is converted depending on the value and selection of area unit. If a specific unit is specified, it returns the value always the same. If a selected unit is not the same type of unit either metric of imperial, it will be ignored, and `auto` will be applied.
+ * @param measureUnitSymbols Optional parameter to provide custom unit symbols
  * @returns  The returning feature will contain `area`,`unit` properties.
  */
 export const calcArea = (
@@ -20,13 +21,13 @@ export const calcArea = (
 	unitType: MeasureUnitType,
 	areaPrecision: number,
 	forceUnit?: forceAreaUnitType,
-	measyreUnitSymbols?: MeasureUnitSymbolType
+	measureUnitSymbols?: MeasureUnitSymbolType
 ) => {
 	if (feature.geometry.type !== 'Polygon') return feature;
 	// caculate area in m2 by using turf/area
 	const result = area(feature.geometry);
 
-	const converted = convertAreaUnit(result, unitType, forceUnit, measyreUnitSymbols);
+	const converted = convertAreaUnit(result, unitType, forceUnit, measureUnitSymbols);
 	converted.area = parseFloat(converted.area.toFixed(areaPrecision));
 
 	feature.properties.area = converted.area;

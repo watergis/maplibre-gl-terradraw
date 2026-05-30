703fb8e688b24d3020ed32d3cb1390e4350b0c1e
Jin Igarashi <igarashi.jin@water-gis.com>
2026-05-31 00:30:40 +0100

    docs: minify esbuild for production documention to get worked (#645)
    * fix: minify by esbuild
    
    * add maplibre-gl to ssr.noExternal again

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	build: {
-		minify: false
+		minify: 'esbuild'
 	},
 	ssr: {
 		noExternal: 'maplibre-gl'
